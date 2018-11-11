728f2f7c6b9c0dc59198be671e8b56b02e32d327
Brandon Jones <brandon@radroot.com>
2018-11-11 17:33:08 -0500

    (demo) Add forceTransparent toggle; refactor

--- demo/demo.js
+++ demo/demo.js
@@ -1,52 +1,39 @@
 const audioUrl = 'YOUR_MP3_FILE.mp3'
 const cdgUrl = 'YOUR_CDG_FILE.cdg'
 
-const app = document.getElementById('app')
-const audio = document.createElement('audio')
-const canvas = document.createElement('canvas')
-const CDGPlayer = require('../index.js')
-const cdg = new CDGPlayer(canvas)
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById('audio')
+  const canvas = document.getElementById('canvas')
 
-// add <canvas> to page
-canvas.width = 600
-canvas.height = 432
-canvas.style.border = '1px solid #ccc'
-app.appendChild(canvas)
-
-// add <audio> to page
-audio.src = audioUrl
-audio.controls = true
-audio.style.display = 'block'
-app.appendChild(audio)
-
-// link to audio element's play/pause events
-audio.addEventListener('play', function () { cdg.play() })
-audio.addEventListener('pause', function () { cdg.stop() })
-
-// sync to audio element's currentTime property
-audio.addEventListener('timeupdate', function () {
-  cdg.sync(audio.currentTime * 1000) // convert to ms
-})
-
-// download and load cdg file
-fetch(cdgUrl)
-  .then(checkStatus)
-  .then(function (response) {
-    return response.arrayBuffer()
-  }).then(function (buffer) {
-    // arrayBuffer to Uint8Array
-    cdg.load(new Uint8Array(buffer))
-  }).catch(function (error) {
-    console.log('request failed', error)
+  const CDGPlayer = require('../index.js')
+  const cdg = new CDGPlayer(canvas, {
+    onBackgroundChange: color => {
+      console.log('onBackgroundChange', color)
+    }
   })
 
-// helper for fetch response
-function checkStatus (response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    var error = new Error(response.statusText)
-    error.response = response
-    throw error
-  }
-}
+  // link to audio events
+  audio.addEventListener('play', () => { cdg.play() })
+  audio.addEventListener('pause', () => { cdg.stop() })
+  audio.addEventListener('timeupdate', () => {
+    cdg.sync(audio.currentTime * 1000) // convert to ms
+  })
+
+  // checkbox for forceTransparent
+  const checkbox = document.getElementById('force_transparent')
+
+  checkbox.addEventListener('change', (e) => {
+    cdg.forceTransparent(e.target.checked)
+  })
+
+  // download and load cdg file
+  fetch(cdgUrl)
+    .then(response => response.arrayBuffer())
+    .then(buffer => {
+      // arrayBuffer to Uint8Array
+      cdg.load(new Uint8Array(buffer))
+
+      // start loading audio
+      audio.src = audioUrl
+    })
+})
